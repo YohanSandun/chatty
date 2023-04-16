f4f0684453927c18966d90b958a1d997ea3744a9
Yohan Sandun Kumaranayake <yohan.sandunk@gmail.com>
2023-04-16 18:25:02 +0530

    Login screen styles

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import './Login.css';
 
 function Login() {
+
+    const [phone, setPhone] = useState('');
+    const [phoneError, setPhoneError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (phone.trim().length === 0) {
+            setPhoneError(true);
+            return false;
+        }
+        
+        setPhoneError(false);
+        return true;
+    }
+
     return (
         <div className="login-page">
-            <div className="login-panel">
-                <label for="phone">Phone Number</label>
-                <input type="phone" name="phone" id="phone" />
-                <button>LOGIN</button>
-            </div>
+            <form className="login-panel" onSubmit={handleSubmit}>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" className={phoneError ? 'error' : ''} value={phone} onChange={(e) => setPhone(e.target.value)} name="phone" id="phone" />
+                <button type="submit">LOGIN</button>
+            </form>
         </div>
     )
 }
